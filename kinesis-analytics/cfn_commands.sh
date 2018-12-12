539a7c6201a1f9331c3e2c9ec56e21428254d702
Karolina Boboli <project@karolinaboboli.com>
2018-12-12 01:07:27 +0100

    Modified application code and stream names

--- kinesis-analytics/cfn_commands.sh
+++ kinesis-analytics/cfn_commands.sh
@@ -5,17 +5,17 @@ aws cloudformation validate-template \
     --profile ${AWS_PROFILE}
 
 aws cloudformation create-stack \
-    --stack-name cs-aws-all-hmk-analytics-kinesis-app \
+    --stack-name metrics-kinesis-app \
     --template-body file://kinesis-analytics/kinesis-application.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
     --profile ${AWS_PROFILE}
 
 aws cloudformation update-stack \
-    --stack-name cs-aws-all-hmk-analytics-kinesis-app \
+    --stack-name metrics-kinesis-app \
     --template-body file://kinesis-analytics/kinesis-application.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
     --profile ${AWS_PROFILE}
 
 aws cloudformation delete-stack \
-    --stack-name cs-aws-all-hmk-analytics-kinesis-app \
+    --stack-name metrics-kinesis-app \
     --profile ${AWS_PROFILE}
